65f6501630543f303696e2088096bb0a19a1568f
Abul Kalam Azad <dev.akazad@hotmail.com>
2021-08-02 15:40:23 +0600

    3d model added

--- js/main.js
+++ js/main.js
@@ -1,167 +1,167 @@
-// import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 
-// // import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
-// import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
-// import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+// import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
+import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
-// let scene,
-//     camera,
-//     renderer,
-//     controls,
-//     PedestalLoader,
-//     artLoader,
-//     monalisaLoader,
-//     chairLoader,
-//     spotLight,
-//     spotLight2,
-//     hemisphereLight,
-//     pointLight1,
-//     pointLight2,
-//     pointLight1Helper,
-//     pointLight2Helper,
-//     axesHelper;
+let scene,
+    camera,
+    renderer,
+    controls,
+    PedestalLoader,
+    artLoader,
+    monalisaLoader,
+    chairLoader,
+    spotLight,
+    spotLight2,
+    hemisphereLight,
+    pointLight1,
+    pointLight2,
+    pointLight1Helper,
+    pointLight2Helper,
+    axesHelper;
 
-// const siteLoader = document.querySelector('.site-loader');
+const siteLoader = document.querySelector('.site-loader');
 
-// function render() {
-//     // INITIATING CONSTRUCTOR
+function render() {
+    // INITIATING CONSTRUCTOR
 
-//     scene = new THREE.Scene();
-//     camera = new THREE.PerspectiveCamera(
-//         60,
-//         (window.innerWidth * 0.5) / (window.innerHeight * 0.7),
-//         1.0,
-//         1000.0
-//     );
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        60,
+        (window.innerWidth * 0.5) / (window.innerHeight * 0.7),
+        1.0,
+        1000.0
+    );
 
-//     // axesHelper = new THREE.AxesHelper(200);
-//     // scene.add(axesHelper);
+    // axesHelper = new THREE.AxesHelper(200);
+    // scene.add(axesHelper);
 
-//     renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-//     renderer.setPixelRatio(window.devicePixelRatio);
-//     renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.7);
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.7);
 
-//     renderer.toneMapping = THREE.ReinhardToneMapping;
-//     renderer.toneMappingExposure = 0.5;
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = 0.5;
 
-//     document
-//         .querySelector('#renderer-container')
-//         .appendChild(renderer.domElement);
+    document
+        .querySelector('#renderer-container')
+        .appendChild(renderer.domElement);
 
-//     // CONTROLS
+    // CONTROLS
 
-//     controls = new OrbitControls(camera, renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);
 
-//     controls.enablePan = false;
-//     controls.enableDamping = true;
-//     controls.maxPolarAngle = 1.37;
-//     controls.minPolarAngle = 1.37;
-//     controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.enableDamping = true;
+    controls.maxPolarAngle = 1.37;
+    controls.minPolarAngle = 1.37;
+    controls.enableZoom = false;
 
-//     camera.position.set(0, 20, 100);
-//     controls.update();
+    camera.position.set(0, 20, 100);
+    controls.update();
 
-//     // LOADER
+    // LOADER
 
-//     PedestalLoader = new FBXLoader();
+    PedestalLoader = new FBXLoader();
 
-//     PedestalLoader.load('../files/PEDESTRAL-2-WEB.fbx', (fbx) => {
-//         fbx.scale.multiplyScalar(0.31);
-//         fbx.rotation.x = -Math.PI / 2;
-//         fbx.position.set(0, 0, 0);
-//         scene.add(fbx);
-//     });
+    PedestalLoader.load('../files/PEDESTRAL-2-WEB.fbx', (fbx) => {
+        fbx.scale.multiplyScalar(0.31);
+        fbx.rotation.x = -Math.PI / 2;
+        fbx.position.set(0, 0, 0);
+        scene.add(fbx);
+    });
 
-//     artLoader = new FBXLoader();
+    artLoader = new FBXLoader();
 
-//     artLoader.load('../files/ART-WEB.fbx', (fbx) => {
-//         fbx.scale.multiplyScalar(0.3);
-//         fbx.rotation.x = -Math.PI / 2;
-//         fbx.position.set(85, 0, -25);
-//         scene.add(fbx);
-//     });
+    artLoader.load('../files/ART-WEB.fbx', (fbx) => {
+        fbx.scale.multiplyScalar(0.3);
+        fbx.rotation.x = -Math.PI / 2;
+        fbx.position.set(85, 0, -25);
+        scene.add(fbx);
+    });
 
-//     monalisaLoader = new FBXLoader();
+    monalisaLoader = new FBXLoader();
 
-//     monalisaLoader.load('../files/MONA-LISA-WEB.fbx', (fbx) => {
-//         fbx.scale.multiplyScalar(0.3);
-//         fbx.rotation.x = -Math.PI / 2;
-//         fbx.rotation.z = -Math.PI / 2;
-//         fbx.position.set(-100, -10, 0);
-//         scene.add(fbx);
-//     });
+    monalisaLoader.load('../files/MONA-LISA-WEB.fbx', (fbx) => {
+        fbx.scale.multiplyScalar(0.3);
+        fbx.rotation.x = -Math.PI / 2;
+        fbx.rotation.z = -Math.PI / 2;
+        fbx.position.set(-100, -10, 0);
+        scene.add(fbx);
+    });
 
-//     chairLoader = new FBXLoader();
+    chairLoader = new FBXLoader();
 
-//     chairLoader.load(
-//         '../files/LOUNGE-CHAIR-WEB.fbx',
-//         (fbx) => {
-//             fbx.scale.multiplyScalar(0.3);
-//             fbx.rotation.x = -Math.PI / 2;
-//             fbx.position.set(5, 1, 5);
-//             scene.add(fbx);
-//         },
-//         (xhr) => {
-//             if ((xhr.loaded / xhr.total) * 100 === 100) {
-//                 console.log('done');
-//                 // siteLoader.style.opacity = 0;
-//                 // siteLoader.style.display = 'none';
-//             }
-//         }
-//     );
+    chairLoader.load(
+        '../files/LOUNGE-CHAIR-WEB.fbx',
+        (fbx) => {
+            fbx.scale.multiplyScalar(0.3);
+            fbx.rotation.x = -Math.PI / 2;
+            fbx.position.set(5, 1, 5);
+            scene.add(fbx);
+        },
+        (xhr) => {
+            if ((xhr.loaded / xhr.total) * 100 === 100) {
+                console.log('done');
+                // siteLoader.style.opacity = 0;
+                // siteLoader.style.display = 'none';
+            }
+        }
+    );
 
-//     // LIGHTS
+    // LIGHTS
 
-//     hemisphereLight = new THREE.HemisphereLight(0x292929, 0xffffff, 4);
-//     scene.add(hemisphereLight);
+    hemisphereLight = new THREE.HemisphereLight(0x292929, 0xffffff, 4);
+    scene.add(hemisphereLight);
 
-//     spotLight = new THREE.SpotLight(0xd1c7be, 4);
-//     spotLight.position.set(50, 25, 50);
-//     spotLight.castShadow = true;
-//     scene.add(spotLight);
+    spotLight = new THREE.SpotLight(0xd1c7be, 4);
+    spotLight.position.set(50, 25, 50);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
 
-//     spotLight2 = new THREE.SpotLight(0xd1c7be, 4);
-//     spotLight2.position.set(-50, 25, -50);
-//     scene.add(spotLight2);
+    spotLight2 = new THREE.SpotLight(0xd1c7be, 4);
+    spotLight2.position.set(-50, 25, -50);
+    scene.add(spotLight2);
 
-//     // const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-//     // scene.add(spotLightHelper);
+    // const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+    // scene.add(spotLightHelper);
 
-//     // const spotLight2Helper = new THREE.SpotLightHelper(spotLight2);
-//     // scene.add(spotLight2Helper);
+    // const spotLight2Helper = new THREE.SpotLightHelper(spotLight2);
+    // scene.add(spotLight2Helper);
 
-//     pointLight1 = new THREE.PointLight(0x808080, 2);
-//     pointLight1.position.set(0, 50, 100);
-//     scene.add(pointLight1);
+    pointLight1 = new THREE.PointLight(0x808080, 2);
+    pointLight1.position.set(0, 50, 100);
+    scene.add(pointLight1);
 
-//     // const pointLight1SphereSize = 5;
-//     // const pointLight1Helper = new THREE.PointLightHelper(
-//     //     pointLight1,
-//     //     pointLight1SphereSize
-//     // );
-//     // scene.add(pointLight1Helper);
+    // const pointLight1SphereSize = 5;
+    // const pointLight1Helper = new THREE.PointLightHelper(
+    //     pointLight1,
+    //     pointLight1SphereSize
+    // );
+    // scene.add(pointLight1Helper);
 
-//     pointLight2 = new THREE.PointLight(0x808080, 2);
-//     pointLight2.position.set(0, 50, -100);
-//     scene.add(pointLight2);
+    pointLight2 = new THREE.PointLight(0x808080, 2);
+    pointLight2.position.set(0, 50, -100);
+    scene.add(pointLight2);
 
-//     // const pointLight2SphereSize = 5;
-//     // const pointLight2Helper = new THREE.PointLightHelper(
-//     //     pointLight2,
-//     //     pointLight2SphereSize
-//     // );
-//     // scene.add(pointLight2Helper);
+    // const pointLight2SphereSize = 5;
+    // const pointLight2Helper = new THREE.PointLightHelper(
+    //     pointLight2,
+    //     pointLight2SphereSize
+    // );
+    // scene.add(pointLight2Helper);
 
-//     animate();
-// }
+    animate();
+}
 
-// function animate() {
-//     renderer.render(scene, camera);
+function animate() {
+    renderer.render(scene, camera);
 
-//     requestAnimationFrame(animate);
-// }
+    requestAnimationFrame(animate);
+}
 
-// render();
+render();
 
 // GSAP Animation
 
